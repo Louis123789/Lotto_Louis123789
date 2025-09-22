6c1c9b80a5b2b11cdab3274ea1b7cf79e83c5d53
Louis123789 <yuan.950413@gmail.com>
2025-09-22 16:35:13 +0800

    第九次commit (觸控座標)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -5,24 +5,18 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
-import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LottoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Play(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Play(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -42,33 +34,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
-    // Get the current context for showing Toast
     val context = LocalContext.current
-
-    // State to hold the random lucky number
-    var lucky by remember {
-        mutableStateOf((1..100).random())
-    }
+    var lucky by remember { mutableStateOf((1..100).random()) }
 
     Column(
         modifier = modifier
             .fillMaxSize()
-            .clickable { // Added clickable modifier to show a Toast on touch
-                Toast.makeText(context, "螢幕觸控(馬崇恩)", Toast.LENGTH_SHORT).show()
+            .pointerInput(Unit) {
+                detectTapGestures { offset ->
+                    val x = offset.x.toInt()
+                    val y = offset.y.toInt()
+                    Toast
+                        .makeText(context, "觸控座標: X=$x, Y=$y", Toast.LENGTH_SHORT)
+                        .show()
+                }
             },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "樂透數字(1-100)為 $lucky"
-        )
+        Text("樂透數字(1-100)為 $lucky")
 
-        Button(
-            onClick = { lucky = (1..100).random() } // Generate a new random number
-        ) {
+        Button(onClick = { lucky = (1..100).random() }) {
             Text("重新產生樂透碼")
         }
-    }
 
-    Text("吳育安共同編輯程式")
+        Text("吳育安共同編輯程式")
+    }
 }
